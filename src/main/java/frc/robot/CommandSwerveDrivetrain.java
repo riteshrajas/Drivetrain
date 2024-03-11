8cdbcfa3474465cd10687397930221729ba6b053
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-03-11 19:23:25 -0400

    Initial commit

---
+++ src/main/java/frc/robot/CommandSwerveDrivetrain.java
@@ -0,0 +1,121 @@
+package frc.robot;
+
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.Notifier;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.generated.TunerConstants;
+
+
+/**
+ * Class that extends the Phoenix SwerveDrivetrain class and implements subsystem
+ * so it can be used in command-based projects easily.
+ */
+public class CommandSwerveDrivetrain extends SwerveDrivetrain implements Subsystem {
+    private static final double kSimLoopPeriod = 0.005; // 5 ms
+    private Notifier m_simNotifier = null;
+    private double m_lastSimTime;
+    public static final PIDController pid = new PIDController(0.025, .05, .00);
+
+    private final SwerveRequest.ApplyChassisSpeeds autoRequest = new SwerveRequest.ApplyChassisSpeeds();
+
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, double OdometryUpdateFrequency, SwerveModuleConstants... modules) {
+
+        super(driveTrainConstants, OdometryUpdateFrequency, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+
+        pid.setTolerance(.25, 0.05); // allowable angle error
+        pid.enableContinuousInput(0, 360); // it is faster to go 1 degree from 359 to 0 instead of 359 degrees
+        pid.setIntegratorRange(-0.2  , 0.2);
+        pid.setSetpoint(-10); // 0 = apriltag angle
+
+    }
+    public CommandSwerveDrivetrain(SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
+        super(driveTrainConstants, modules);
+        configurePathPlanner();
+        if (Utils.isSimulation()) {
+            startSimThread();
+        }
+    }
+    public boolean getPIDAtSetpoint() {
+        return pid.atSetpoint();
+    }
+
+
+    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+        return run(() -> this.setControl(requestSupplier.get()));
+    }
+
+    public Command getAutoPath(String pathName) {
+        return new PathPlannerAuto(pathName);
+    }
+
+    public ChassisSpeeds getCurrentRobotChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
+    }
+
+
+    private void configurePathPlanner() {
+        double driveBaseRadius = 0;
+        for (var moduleLocation : m_moduleLocations) {
+            driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+        }
+
+        AutoBuilder.configureHolonomic(
+                ()->this.getState().Pose, // Supplier of current robot pose
+                this::seedFieldRelative,  // Consumer for seeding pose against auto
+                this::getCurrentRobotChassisSpeeds,
+                (speeds)->this.setControl(autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
+                new HolonomicPathFollowerConfig(new PIDConstants(10, 0, 0),
+                        new PIDConstants(10, 0, 0),
+                        TunerConstants.kSpeedAt12VoltsMps,
+                        driveBaseRadius,
+                        new ReplanningConfig()),
+                ()->false, // Change this if the path needs to be flipped on red vs blue
+                this); // Subsystem for requirements
+    }
+
+    public double getPIDRotation(double currentX) {
+        return pid.calculate(currentX);
+    }
+
+    public void resetPID() {
+        pid.reset();
+    }
+
+
+    private void startSimThread() {
+        m_lastSimTime = Utils.getCurrentTimeSeconds();
+
+        /* Run simulation at a faster rate so PID gains behave more reasonably */
+        m_simNotifier = new Notifier(() -> {
+            final double currentTime = Utils.getCurrentTimeSeconds();
+            double deltaTime = currentTime - m_lastSimTime;
+            m_lastSimTime = currentTime;
+
+            /* use the measured time delta, get battery voltage from WPILib */
+            updateSimState(deltaTime, RobotController.getBatteryVoltage());
+        });
+        m_simNotifier.startPeriodic(kSimLoopPeriod);
+    }
+}
---
+++ src/main/java/frc/robot/DriveForwardForTime.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class DriveForwardForTime extends SequentialCommandGroup {
+		/** Creates a new DriveForwardForTime. */
+		private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric()
+				.withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+		private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+
+		public DriveForwardForTime(CommandSwerveDrivetrain swerve, double time) {
+				// Add your commands in the addCommands() call, e.g.
+				// addCommands(new FooCommand(), new BarCommand());
+				addCommands(
+						new ParallelDeadlineGroup(
+								new WaitCommand(time),
+								swerve.applyRequest(
+										() -> forwardStraight
+												.withVelocityX(0.1)
+												.withVelocityY(0)))
+								.andThen(new ParallelDeadlineGroup(new WaitCommand(5), swerve.applyRequest(() -> brake))));
+		}
+}
